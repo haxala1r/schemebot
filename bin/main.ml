45e5507ecebd404d5063c8cc10af8ab49f5ab52c
Emin Arslan <me@emin.engineer>
2026-05-29 17:47:36 +0300

    switched to cohttp, now have a hello-world server running.

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,17 @@
-let () = print_endline "Hello, World!"
+
+open Lwt
+open Cohttp_lwt_unix
+
+
+let handle _conn _req _body =
+  Server.respond_string ~status:`OK ~body:"Hello world!" ()
+
+let server =
+  Server.create ~mode:(`TCP (`Port (int_of_string (Sys.getenv "PORT"))))
+(Server.make ~callback:handle ())
+
+
+
+let _ =
+  print_endline "starting server";
+  Lwt_main.run server
--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
           version = "0.0.1";
           src = ./.;
 
-          buildInputs = with pkgs.ocamlPackages; [ocsigen_server];
+          buildInputs = with pkgs.ocamlPackages; [findlib cohttp-lwt-unix lwt];
         };
       shell = pkgs:
         pkgs.mkShell {
